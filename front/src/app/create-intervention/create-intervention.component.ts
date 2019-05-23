b75bd6eadae98ae29dd71360c101e255eda6d073
quentin basset <quentin.basset@ochelys.com>
2019-05-23 15:41:33 +0200

    :sparkles: add CreateInterventionComponent

---
+++ front/src/app/create-intervention/create-intervention.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-intervention',
+  templateUrl: './create-intervention.component.html',
+  styleUrls: ['./create-intervention.component.scss']
+})
+export class CreateInterventionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
