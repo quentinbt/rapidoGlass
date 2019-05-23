26f07b6adc5eb0a068b0690689dcdd518dfbe3dc
quentin basset <quentin.basset@ochelys.com>
2019-05-23 23:06:57 +0200

    :sparkles: add obs hasSignedIn to LoginComponent

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -21,19 +21,20 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    if (this.userService.hasSignedIn()) {
-      this.router.navigate(['/']);
-    }
+    this.userService.getHasSignedIn().subscribe((hasSignedIn: boolean) => {
+      hasSignedIn && this.router.navigate(['/'])
+    })
+
     this.loginForm = this.formBuilder.group({
       login: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     })
+
     this.matcher = new MyErrorStateMatcher()
   }
 
   public onSubmit() {
     if (!this.loginForm.valid) {
-      console.log(this.loginForm)
       return
     }
     this.userService.signIn(this.loginForm.value)
