db138ebe5c10e6d1c4068ff91aed1fdba88531f1
quentin basset <quentin.basset@ochelys.com>
2019-06-04 22:32:46 +0200

    :sparkles: update interface Intervention

--- front/src/app/interfaces/intervention.ts
+++ front/src/app/interfaces/intervention.ts
@@ -1,5 +1,9 @@
 export interface Intervention {
   id?: number
-  description: string
+  description?: string
   planned_at?: string
+  created_at?: string
+  plate_number?: string
+  car_model?: string
+  insurance_number?: string
 }
