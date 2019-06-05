94c2154e285499c3a5453032abec11dc4e4eedbb
quentin basset <quentin.basset@ochelys.com>
2019-06-05 22:53:53 +0200

    :sparkles: hide / show planned button if or not admin

--- front/src/app/intervention/intervention.component.ts
+++ front/src/app/intervention/intervention.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { InterventionService } from '../services/intervention.service'
+import { UserService } from '../services/user.service'
 import { Intervention } from '../interfaces/intervention'
+import { User } from '../interfaces/user'
 
 @Component({
   selector: 'app-intervention',
@@ -10,15 +12,20 @@ import { Intervention } from '../interfaces/intervention'
 })
 export class InterventionComponent implements OnInit {
   public intervention: Intervention
+  public currentUser: User
 
   constructor(
     private interventionService: InterventionService,
+    private userService: UserService,
     private route: ActivatedRoute,
   ) {
     this.route.params.subscribe(params => {
       if (params.id) {
         this.getIntervention(params.id)
       }
+    })
+    this.userService.getCurrentUser().subscribe((currentUser: User) => {
+      this.currentUser = currentUser
     })
   }
     
