82eeb68428495c7239648b47c3e66f09d4944ad0
quentin basset <quentin.basset@ochelys.com>
2019-06-08 21:05:52 +0200

    :sparkles: add toastr interation on InterventionService

--- front/src/app/services/intervention.service.ts
+++ front/src/app/services/intervention.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Intervention } from '../interfaces/intervention'
 import { Observable } from 'rxjs'
+import { ToastrService } from 'ngx-toastr'
+import { tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
 })
 export class InterventionService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService
+  ) {}
 
   public getAll(): Observable<Intervention[]> {
     const url = 'interventions.json'
@@ -17,6 +22,11 @@ export class InterventionService {
   public create(intervention: Intervention): Observable<Intervention> {
     const url = 'interventions.json'
     return this.http.post<Intervention>(url, intervention)
+    .pipe(
+      tap(res => {
+        this.toastr.success('', "Demande d'intervention enregistré!")
+      })
+    )
   }
 
   public show(interventionId: number): Observable<Intervention> {
@@ -27,5 +37,11 @@ export class InterventionService {
   public update(intervention: Intervention): Observable<Intervention> {
     const url = `interventions/${intervention.id}.json`
     return this.http.put<Intervention>(url, intervention)
+    .pipe(
+      tap(res => {
+        this.toastr.success('', 'Modification enregistré!')
+      })
+    )
+
   }
 }
